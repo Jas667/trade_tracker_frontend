279a3b6d9f7f39fa5fa16a7a21a5995c2dfd91c1
Jas667 <100931408+Jas667@users.noreply.github.com>
2023-09-23 10:24:09 +0100

    styling fixed but still tag add issues

--- src/components/TradeFilterBar.jsx
+++ src/components/TradeFilterBar.jsx
@@ -1,13 +1,42 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
-function TradeFilterBar({ onFilter, startDate, endDate }) {
+function TradeFilterBar({ onFilter, startDate, endDate, initialTags }) {
   // Local state for the filters
   const [localStartDate, setLocalStartDate] = useState(startDate);
   const [localEndDate, setLocalEndDate] = useState(endDate);
   const [localSymbol, setLocalSymbol] = useState("");
+  //for tags
+  const [tags, setTags] = useState(["tag1", "tag2", "tag3", "tag4", "tag5"]);
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const wrapperRef = useRef(null);
 
   const handleFilter = () => {
     onFilter(localStartDate, localEndDate, localSymbol); // send the filter criteria back to parent
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (event) => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      setShowDropdown(false);
+    }
+  };
+
+  const addTag = (tag) => {
+    setSelectedTags((prev) => [...prev, tag]);
+    setTags(tags.filter((t) => t !== tag));
+    setShowDropdown(false);
+  };
+
+  const removeTag = (tag) => {
+    setSelectedTags(selectedTags.filter((t) => t !== tag));
+    setTags((prev) => [...prev, tag]);
   };
 
   return (
@@ -51,12 +80,42 @@ function TradeFilterBar({ onFilter, startDate, endDate }) {
           <label htmlFor="selectTags" className="mr-2">
             Select Tags:
           </label>
-          <input
-            type="text"
-            id="selectTags"
-            placeholder="Select Tags"
-            className="form-input flex-grow p-1 rounded bg-gray-700 text-white"
-          />
+          <div className="relative flex-grow" ref={wrapperRef}>
+            <input
+              type="text"
+              id="selectTags"
+              placeholder="Select Tags"
+              className="form-input flex-grow p-1 rounded bg-gray-700 text-white"
+              onClick={() => setShowDropdown(true)}
+              readOnly
+            />
+            {showDropdown && (
+              <div className="absolute w-full mt-1 bg-gray-700 border z-10">
+                {tags.map((tag) => (
+                  <div
+                    key={tag}
+                    onClick={() => addTag(tag)}
+                    className="p-2 hover:bg-gray-900 cursor-pointer"
+                  >
+                    {tag}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="flex ml-2">
+          {selectedTags.map((tag) => (
+            <div key={tag} className="mr-2 bg-blue-500 p-1 rounded text-white">
+              {tag}
+              <span
+                className="ml-2 cursor-pointer"
+                onClick={() => removeTag(tag)}
+              >
+                x
+              </span>
+            </div>
+          ))}
         </div>
 
         {/* Date Range, Filter & Clear sections */}
@@ -93,7 +152,10 @@ function TradeFilterBar({ onFilter, startDate, endDate }) {
 
           {/* Filter & Clear Buttons Group */}
           <div className="flex items-center">
-            <button className="btn bg-blue-600 hover:bg-blue-500 text-white border border-blue-400 p-1 rounded mr-2" onClick={handleFilter}>
+            <button
+              className="btn bg-blue-600 hover:bg-blue-500 text-white border border-blue-400 p-1 rounded mr-2"
+              onClick={handleFilter}
+            >
               Filter
             </button>
             <button className="btn bg-red-600 hover:bg-red-500 text-white border border-red-400 p-1 rounded">
