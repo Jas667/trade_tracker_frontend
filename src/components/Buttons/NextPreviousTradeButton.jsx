d6f43c261762638afef8a3d614f2d2ad3d5fd798
Jas667 <100931408+Jas667@users.noreply.github.com>
2023-11-02 10:37:43 +0000

    tooltip added to next/prev

--- src/components/Buttons/NextPreviousTradeButton.jsx
+++ src/components/Buttons/NextPreviousTradeButton.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
+import Tooltip from "react-bootstrap/Tooltip";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 function NextPreviousTradeButton({
   rawTradeData,
@@ -27,24 +29,64 @@ function NextPreviousTradeButton({
     }
   };
 
+  const renderTooltipNoPrev = (props) => (
+    <Tooltip id="button-tooltip-prev" {...props}>
+      You are at the beginning of selected trades
+    </Tooltip>
+  );
+
+  const renderTooltipNoNext = (props) => (
+    <Tooltip id="button-tooltip-next" {...props}>
+      You are at the end of selected trades
+    </Tooltip>
+  );
+
   return (
     <div className="flex justify-between mt-4">
-      <Button
-        onClick={handlePrevious}
-        className="max-w-md mb-2"
-        variant="secondary"
-        disabled={currentIndex === 0}
-      >
-        ← Prev
-      </Button>
-      <Button
-        onClick={handleNext}
-        className="max-w-md mb-2"
-        variant="secondary"
-        disabled={currentIndex === rawTradeData.length - 1}
-      >
-        Next →
-      </Button>
+      {currentIndex === 0 ? (
+        <OverlayTrigger placement="bottom" overlay={renderTooltipNoPrev}>
+          <span className="d-inline-block">
+            <Button
+              onClick={handlePrevious}
+              className="max-w-md mb-2"
+              variant="secondary"
+              disabled
+            >
+              ← Prev
+            </Button>
+          </span>
+        </OverlayTrigger>
+      ) : (
+        <Button
+          onClick={handlePrevious}
+          className="max-w-md mb-2"
+          variant="secondary"
+        >
+          ← Prev
+        </Button>
+      )}
+      {currentIndex === rawTradeData.length - 1 ? (
+        <OverlayTrigger placement="bottom" overlay={renderTooltipNoNext}>
+          <span className="d-inline-block">
+            <Button
+              onClick={handleNext}
+              className="max-w-md mb-2"
+              variant="secondary"
+              disabled
+            >
+              Next →
+            </Button>
+          </span>
+        </OverlayTrigger>
+      ) : (
+        <Button
+          onClick={handleNext}
+          className="max-w-md mb-2"
+          variant="secondary"
+        >
+          Next →
+        </Button>
+      )}
     </div>
   );
 }
