000e5332de9e97e1e77a4a6cbc11628549ae68c6
Jas667 <100931408+Jas667@users.noreply.github.com>
2023-11-13 10:03:40 +0000

    remove / from api call

--- src/services/apiTestService.jsx
+++ src/services/apiTestService.jsx
@@ -2,7 +2,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const testApi = async () => { 
       try {
-      const response = await fetch(`${API_BASE_URL}/ip`, {
+      const response = await fetch(`${API_BASE_URL}ip`, {
             method: "GET",
             credentials: "include",
       });
